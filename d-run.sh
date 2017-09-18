225055a7ed0d04f90436c059f650415004d75817
Pierre <pierre@pvln.nl>
2017-09-18 21:41:35 +0000

    updated from example

--- d-run.sh
+++ d-run.sh
@@ -3,7 +3,7 @@
 # reads and executes commands from filename in the current shell environment
 source ./settings/bld.sh
 source ./settings/run.sh
-#source ./settings/volumes.sh
+source ./settings/volumes.sh
 
 # inspiration: https://stackoverflow.com/questions/38576337/execute-bash-command-if-docker-container-does-not-exist
 #
@@ -16,14 +16,15 @@ if [ ! "$(docker ps -q -f name=$my_container_name)" ]; then
     fi
 
     # run your container with volume mappping
-    #docker run -it \
-#	  --name $my_container_name \
-#	  -v $MY_MYSQL_DATA_VOLUME:/var/lib/mysql \
-#	  $my_build_name
+    docker run -it \
+	  --name $my_container_name \
+	  -v $MY_MYSQL_DATA_VOLUME:/var/lib/mysql \
+          -e MYSQL_ROOT_PASSWORD=root \
+	  $my_build_name
 
     # run your container without volume mapping
-    docker run -it \
-          --name $my_container_name \
-          $my_build_name
+    #docker run -it \
+    #      --name $my_container_name \
+    #      $my_build_name
 
 fi
