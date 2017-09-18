f7f713ad6b77323c81bdc50526e95a328dd5383d
Pierre <pierre@pvln.nl>
2017-09-18 11:13:40 +0000

    volumes removed

--- d-run.sh
+++ d-run.sh
@@ -3,7 +3,7 @@
 # reads and executes commands from filename in the current shell environment
 source ./settings/bld.sh
 source ./settings/run.sh
-source ./settings/volumes.sh
+#source ./settings/volumes.sh
 
 # inspiration: https://stackoverflow.com/questions/38576337/execute-bash-command-if-docker-container-does-not-exist
 #
@@ -14,9 +14,16 @@ if [ ! "$(docker ps -q -f name=$my_container_name)" ]; then
         # cleanup container first
         docker rm $my_container_name
     fi
-    # run your container
+
+    # run your container with volume mappping
+    #docker run -it \
+#	  --name $my_container_name \
+#	  -v $MY_MYSQL_DATA_VOLUME:/var/lib/mysql \
+#	  $my_build_name
+
+    # run your container without volume mapping
     docker run -it \
-	  --name $my_container_name \
-	  -v $MY_MYSQL_DATA_VOLUME:/var/lib/mysql \
-	  $my_build_name
+          --name $my_container_name \
+          $my_build_name
+
 fi
