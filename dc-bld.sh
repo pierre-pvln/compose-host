4eb8b3c6ac12fdcd94dd815e4514e79535a82940
Pierre <pierre@pvln.nl>
2017-10-01 18:37:41 +0000

    scripts added

--- dc-bld.sh
+++ dc-bld.sh
@@ -2,14 +2,6 @@
 source ./settings/volumes.sh
 source ./settings/platform.sh
 
-# - _ and . are ignored
-MY_PROJECT_NAME="pvlnwebsite"
-
-env
-echo =================================================
-set
-
 docker-compose --project-name ${MY_PROJECT_NAME} \
-               --file ../integrations/pvln.nl.yml \
+               --file ../integrations/${MY_YAML_FILE} \
                build
-
--- dc-cfg.sh
+++ dc-cfg.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 source ./settings/volumes.sh
+source ./settings/platform.sh
 
-docker-compose --project-name mysql-tst-project --file ../integrations/pvln.nl.yml config --services --volumes
-
-
-
-
+docker-compose --project-name ${MY_PROJECT_NAME} \
+               --file ../integrations/${MY_YAML_FILE} \
+               config --services --volumes
