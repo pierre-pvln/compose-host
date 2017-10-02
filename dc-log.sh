3465326339efb55fb4293be26f2ad85dd802237e
Pierre <pierre@pvln.nl>
2017-10-02 19:45:03 +0000

    logging added

---
+++ dc-log.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source ./settings/volumes.sh
+source ./settings/platform.sh
+
+docker-compose --project-name ${MY_PROJECT_NAME} \
+               --file ../integrations/${MY_YAML_FILE} \
+               logs
---
+++ pvln-log.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# - _ and . are ignored
+export MY_PROJECT_NAME="pvlnserver"
+export MY_YAML_FILE="pvln.nl.yml"
+
+source ./dc-log.sh
