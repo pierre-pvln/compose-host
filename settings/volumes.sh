f5ee62fb328b77ba42d6c26219dc6d4482b419ca
Pierre <pierre@pvln.nl>
2017-10-04 20:33:46 +0000

    web volume added

--- settings/volumes.sh
+++ settings/volumes.sh
@@ -19,3 +19,13 @@ echo "==============="
 echo "FTP data volume: " $MY_FTP_DATA_VOLUME
 echo "==============="
 
+export MY_WEB_DATA_VOLUME='/home/pi/docker/compose/storage/www'
+if [ ! -d "$MY_WEB_DATA_VOLUME" ]; then
+  # Will enter here if $MY_WEB_DATA_VOLUME doesn't exist.
+  mkdir -p $MY_WEB_DATA_VOLUME
+fi
+
+echo "==============="
+echo "WWW data volume: " $MY_WEB_DATA_VOLUME
+echo "==============="
+
