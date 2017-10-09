02f21a5ad57a086a302ac7dcd90839ac7762174f
Pierre <pierre@pvln.nl>
2017-10-09 19:04:10 +0000

    changed

--- dc-bld.sh
+++ dc-bld.sh
@@ -2,6 +2,8 @@
 source ./settings/volumes.sh
 source ./settings/platform.sh
 
+echo docker-compose --project-name ${MY_PROJECT_NAME} --file ../integrations/${MY_LEVEL}/${MY_YAML_FILE} build
+
 docker-compose --project-name ${MY_PROJECT_NAME} \
-               --file ../integrations/${MY_YAML_FILE} \
+               --file ../integrations/${MY_LEVEL}/${MY_YAML_FILE} \
                build
--- pvln-bld.sh
+++ pvln-bld.sh
@@ -3,5 +3,6 @@
 # - _ and . are ignored
 export MY_PROJECT_NAME="pvlnserver"
 export MY_YAML_FILE="pvln.nl.yml"
+export MY_LEVEL="lvl2"
 
 source ./dc-bld.sh
