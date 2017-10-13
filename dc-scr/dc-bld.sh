39931ffb3aa1a5b9f4336c65fd68c7d1cab08974
Pierre <pierre@pvln.nl>
2017-10-13 20:36:44 +0000

    folder structure changed

--- dc-scr/dc-bld.sh
+++ dc-scr/dc-bld.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-#source ./settings/volumes.sh
-#source ./settings/platform.sh
-
-echo docker-compose --project-name ${MY_PROJECT_NAME} --file ../integrations/${MY_LEVEL}/${MY_YAML_FILE} build
+echo docker-compose --project-name ${MY_PROJECT_NAME} --file ../../../../stacks/${MY_LEVEL}/${MY_YAML_FILE} build
 
 docker-compose --project-name ${MY_PROJECT_NAME} \
-               --file ../integrations/${MY_LEVEL}/${MY_YAML_FILE} \
+               --file ../../../../stacks/${MY_LEVEL}/${MY_YAML_FILE} \
                build
--- stacks/lvl1/mysql/mysql-bld.sh
+++ stacks/lvl1/mysql/mysql-bld.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# - _ and . are ignored
-#export MY_PROJECT_NAME="tstmysql"
-#export MY_YAML_FILE="mysql-tst.yml"
-
 source ./settings.sh
 
 source ../../../dc-scr/dc-bld.sh
