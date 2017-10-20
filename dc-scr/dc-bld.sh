f2e5fe62c727dedb4cb30b0e2a6509ffa05e8fa2
Pierre <pierre@pvln.nl>
2017-10-20 22:25:47 +0200

    changes

--- dc-scr/dc-bld.sh
+++ dc-scr/dc-bld.sh
@@ -5,6 +5,11 @@
 #    ${MY_YAML_FILE}    = path to the YAML file including YAML file name
 #    ${MY_LEVEL}        = the level of the stack/integration (lvl1, lvl2, lvl3)
 
+
+#If MY_APACHE2_SITENAME is not defined set it to default value: html
+export MY_APACHE2_SITENAME="${MY_APACHE2_SITENAME:-html}"
+
+
 echo docker-compose --project-name ${MY_PROJECT_NAME} --file ../../../../stacks/${MY_LEVEL}/${MY_YAML_FILE} build
 
 docker-compose --project-name ${MY_PROJECT_NAME} \
