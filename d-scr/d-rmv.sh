70a4e3799a04e057a4bb49e7b5dd82a510e28d0c
Pierre <pierre@pvln.nl>
2017-10-20 20:51:47 +0000

    chmodded

--- d-scr/d-rmv.sh
+++ d-scr/d-rmv.sh
@@ -4,10 +4,10 @@
 #
 
 #Remove all exited containers
-docker rm $(docker ps -a -f status=exited -q)
+docker rm -f $(docker ps -a -f status=exited -q)
 
 #Remove the images
-docker rmi $(docker images  -a -q)
+docker rmi -f $(docker images  -a -q)
 
 # Remove all dangling volumes
 docker volume rm $(docker volume ls -f dangling=true -q)
