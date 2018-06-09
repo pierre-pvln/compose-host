702a924a90d081c21576d8f8df640288f8c31171
Pierre <pierre@pvln.nl>
2018-06-09 19:31:21 +0200

    updated

--- stacks/lvl1/voluntas/settings.sh
+++ stacks/lvl1/voluntas/settings.sh
@@ -2,15 +2,12 @@
 
 # Required generic variables
 #
-# IMPORTANT: - _ and . in the variablename are ignored so don't use them
+# IMPORTANT: - _ and . in the variable name are ignored so don't use them
 
 export MY_PROJECT_NAME="devvoluntas"
 export MY_YAML_FILE="voluntas.yml"
-export MY_PLATFORM="rpi3"
-export MY_LEVEL="lvl1"
 
-
-source ./settings/bld.sh
+source ./settings/build.sh
 source ./settings/mysql.sh
 source ./settings/platform.sh
 source ./settings/run.sh
