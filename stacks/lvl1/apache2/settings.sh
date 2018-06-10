34fe0081afa08aad3708029aa854d5409975c948
Pierre Veelen <pierre@pvln.nl>
2018-06-10 14:26:36 +0200

    Update settings.sh

--- stacks/lvl1/apache2/settings.sh
+++ stacks/lvl1/apache2/settings.sh
@@ -8,9 +8,8 @@ export MY_PROJECT_NAME="devapache2"
 export MY_YAML_FILE="apache2.yml"
 
 source ./settings/builder.sh
-source ./settings/mysql.sh
 source ./settings/platform.sh
-source ./settings/run.sh
-source ./settings/volumes.sh
+source ./settings/container.sh
 source ./settings/level.sh
-
+source ./settings/mysql.sh
+source ./settings/volumes.sh
