97ae91f9fb1f8f4bd9cf4c899096818560c49e25
Pierre <pierre@pvln.nl>
2018-06-11 14:08:08 +0200

    cli added

---
+++ dc-scr/dc-cli.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Required environment variables:
+#    ${MY_PROJECT_NAME} = the name of the project
+#    ${MY_YAML_FILE}    = path to the YAML file including YAML file name
+#    ${MY_LEVEL}        = the level of the stack/integration (lvl1, lvl2, lvl3)
+
+echo docker-compose --project-name ${MY_PROJECT_NAME} --file ../../../../integrations/${MY_LEVEL}/${MY_YAML_FILE} exec /bin/bash
+
+docker-compose --project-name ${MY_PROJECT_NAME} \
+               --file ../../../../integrations/${MY_LEVEL}/${MY_YAML_FILE} \
+               exec /bin/bash
