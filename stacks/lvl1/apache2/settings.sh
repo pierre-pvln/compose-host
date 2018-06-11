90a510ce68186c23f049c8cdfa0dc9f84a101f4e
Pierre <pierre@pvln.nl>
2018-06-11 18:32:50 +0200

    volumes split

--- stacks/lvl1/apache2/settings.sh
+++ stacks/lvl1/apache2/settings.sh
@@ -9,9 +9,20 @@ export MY_YAML_FILE="apache2.yml"
 
 export MY_ASSUMED_CONTAINER=$MY_PROJECT_NAME"_apache2server_1"
 
+# Generic settings for the build / container
 source ./settings/builder.sh
 source ./settings/platform.sh
 source ./settings/container.sh
 source ./settings/level.sh
-source ./settings/volumes.sh
-source ./settings/mysql.sh
+
+# Settings for data web-volume
+source ./settings/vol_www.sh
+
+# Settings for data ftp-volume
+#source ./settings/vol_ftp.sh
+
+# settings for data  mysql volume 
+#source ./settings/vol_mysql.sh
+
+# additional mysql settings
+#source ./settings/mysql.sh
