61e382c9e80ff129b4ac2bd5239330144ca4da2a
Pierre <pierre@pvln.nl>
2018-06-12 18:07:15 +0200

    updated

--- stacks/lvl1/apache2/up-it.sh
+++ stacks/lvl1/apache2/up-it.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Get relevant settings
+#
 source ./settings.sh
+
+# copy default data to web data volume
+cp --recursive ../../../../services/"${MY_PLATFORM}"/"${MY_CONTAINER_NAME}"/files/default/* "${MY_WEB_DATA_VOLUME}"
 
 source ../../../dc-scr/dc-up.sh
