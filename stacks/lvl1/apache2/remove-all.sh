99f8f3318104b41d591545d801eabf47dedc2a1d
Pierre Veelen <pierre@pvln.nl>
2018-06-17 13:57:56 +0200

    Update remove-all.sh

--- stacks/lvl1/apache2/remove-all.sh
+++ stacks/lvl1/apache2/remove-all.sh
@@ -4,11 +4,13 @@ source ./settings.sh
 
 # list all the container id's
 #
-echo "list alle the container id's for "${MY_PROJECT_NAME}
+echo "List all the container id's for "${MY_PROJECT_NAME}
 MY_CONTAINERLIST=$(ps -a -q --filter=name=${MY_PROJECT_NAME}* --filter=status=exited)
 echo MY_CONTAINERLIST
+echo "Stop all the container id's for "${MY_PROJECT_NAME}
+
 
 #Remove the containers when they are exited 
-docker container rm -f $(docker ps -a -q --filter=name=${MY_PROJECT_NAME}* --filter=status=exited)
+#docker container rm -f $(docker ps -a -q --filter=name=${MY_PROJECT_NAME}* --filter=status=exited)
 echo $?
 
