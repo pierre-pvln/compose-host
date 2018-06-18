5d9c5a9c426111eb2fa28edd5d45bc94ef60b6ae
Pierre <pierre@pvln.nl>
2018-06-18 14:36:16 +0200

    auto update

--- stacks/lvl1/mysql/settings.sh
+++ stacks/lvl1/mysql/settings.sh
@@ -9,6 +9,7 @@ export MY_YAML_FILE="mysql_5.5.yml"
 export MY_DOCKERFILE="Dockerfile-clean"
 
 export MY_ASSUMED_CONTAINER=$MY_PROJECT_NAME"_mysql_1"
+
 source ./settings/builder.sh
 source ./settings/platform.sh
 source ./settings/container.sh
