a968c2bf2b48d79d9458f70d92fb9fdbe5e2c987
Pierre <pierre@pvln.nl>
2018-06-18 22:45:05 +0200

    update

--- stacks/lvl1/mysql/default_data.sh
+++ stacks/lvl1/mysql/default_data.sh
@@ -3,4 +3,5 @@
 # copy default data to web data volume
 #
 echo "Copying default data to container volume"
-cp --recursive ../../../../services/"${MY_PLATFORM}"/"${MY_CONTAINER_NAME}"/files/default/* "${MY_WEB_DATA_VOLUME}"
+sudo cp --recursive ../../../../services/"${MY_PLATFORM}"/"${MY_CONTAINER_NAME}"/files/default/* "${MY_WEB_DATA_VOLUME}"
+sudo chown -R www-data:www-data "${MY_WEB_DATA_VOLUME}" 
