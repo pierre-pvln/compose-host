35462c6582816fda5989aaaa77d872b012577949
Pierre <pierre@pvln.nl>
2018-06-18 16:21:51 +0200

    logfile added

--- stacks/lvl1/php5/bld-it.sh
+++ stacks/lvl1/php5/bld-it.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+LOG_ALSO_TO_FILE="YES"
+MY_LOG_FOLDER="./logs"
 
-source ./settings.sh
+if [ ${LOG_ALSO_TO_FILE}=="YES"]; then
+    MY_CURRENT_DATETIME=$(date +"%Y%m%d_%H%M%S")
+	[ ! -d ${MY_LOG_FOLDER} ] mkdir -p ${MY_LOG_FOLDER}
+	MY_LOG_FILE=${MY_LOG_FOLDER}"/"${MY_CURRENT_DATETIME}"_logfile"
+    echo "===================================="
+    echo "Starting logging to "${MY_LOG_FILE}" ..."
+    source ./settings.sh 2>&1 | tee $MY_LOG_FILE
+    source ../../../dc-scr/dc-bld.sh 2>&1 | tee $MY_LOG_FILE
 
-source ../../../dc-scr/dc-bld.sh
+else
+    source ./settings.sh
+    source ../../../dc-scr/dc-bld.sh
+fi
