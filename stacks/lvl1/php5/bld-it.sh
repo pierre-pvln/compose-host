089d6c7d7e0a1677d5870378665efe3ea4618c39
Pierre <pierre@pvln.nl>
2018-06-18 17:26:09 +0200

    auto update

--- stacks/lvl1/php5/bld-it.sh
+++ stacks/lvl1/php5/bld-it.sh
@@ -18,7 +18,7 @@ if [ ${LOG_ALSO_TO_FILE} == "YES" ]
 then
     MY_CURRENT_DATETIME=$(date +"%Y%m%d_%H%M%S");
     [ ! -d ${MY_LOG_FOLDER} ] && mkdir -p ${MY_LOG_FOLDER};
-    MY_LOG_FILE=${MY_LOG_FOLDER}"/"${MY_CURRENT_DATETIME}"_logfile"
+    MY_LOG_FILE=${MY_LOG_FOLDER}"/"${MY_CURRENT_DATETIME}"_bld-it.log"
     echo "===================================="
     echo "Starting logging to "${MY_LOG_FILE}" ..."
     echo
