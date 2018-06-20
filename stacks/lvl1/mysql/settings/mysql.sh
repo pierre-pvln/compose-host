0bd73af4b9f429a23ff228c224d4d66343b9213d
Pierre <pierre@pvln.nl>
2018-06-20 23:52:47 +0200

    auto update

--- stacks/lvl1/mysql/settings/mysql.sh
+++ stacks/lvl1/mysql/settings/mysql.sh
@@ -7,7 +7,7 @@ export MY_SQL_USER_PW='joomla'
 
 echo "==============="
 echo "MySQL root password: " $MY_SQL_ROOT_PW
-echo "MySQL databse      : " $MY_SQL_DATABASE
-echo "MySQL user         : " $MY_SQL_USER
+echo "MySQL database name: " $MY_SQL_DATABASE
+echo "MySQL user name    : " $MY_SQL_USER
 echo "MySQL user password: " $MY_SQL_USER_PW
 echo "==============="
