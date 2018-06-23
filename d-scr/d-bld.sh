10e1be512f66bf98460e5b19c48e30512650c223
Pierre <pierre@pvln.nl>
2018-06-23 10:41:38 +0200

    auto update

--- d-scr/d-bld.sh
+++ d-scr/d-bld.sh
@@ -2,14 +2,16 @@
 #
 # Required generic environment variables:
 #    ${MY_BUILDER}         = the name of the builder (pvln)
-#    ${MY_PLATFORM}        = type of platform to build it for (rpi3) 
+#    ${MY_PLATFORM}        = type of platform to build it for (rpi3)
 #    ${MY_LEVEL}           = building level (should be lvl0)
 #    ${MY_CONTAINER_NAME}  = name of the container (baseline)
-#    ${MY_VERSION}         = version (1.0) 
+#    ${MY_VERSION}         = version (1.0)
 
 echo ========
-echo Running: docker build --tag ${MY_BUILDER}/${MY_PLATFORM}-${MY_LEVEL}-${MY_CONTAINER_NAME}:${MY_VERSION} --file ../../services/${MY_PLATFORM}/${MY_CONTAINER_NAME}/${MY_DOCKERFILE} --no-cache
+echo Running: docker build --no-cache --rm --tag ${MY_BUILDER}/${MY_PLATFORM}-${MY_LEVEL}-${MY_CONTAINER_NAME}:${MY_VERSION} --file ../../../../services/${MY_PLATFORM}/${MY_CONTAINER_NAME}/${MY_DOCKERFILE} ../../../../services/${MY_PLATFORM}/${MY_CONTAINER_NAME}
 echo ========
+
+ls  ../../../../services/${MY_PLATFORM}/${MY_CONTAINER_NAME}
 
 # relevant build options
 #
@@ -17,8 +19,7 @@ echo ========
 # --rm:       Remove intermediate containers after a successful build
 
 # use environment variables during build
-docker build \ 
+docker build --no-cache --rm \
        --tag ${MY_BUILDER}/${MY_PLATFORM}-${MY_LEVEL}-${MY_CONTAINER_NAME}:${MY_VERSION} \
-	   --file ../../services/${MY_PLATFORM}/${MY_CONTAINER_NAME}/${MY_DOCKERFILE} \
-	   --no-cache
-	   --rm
+       --file ../../../../services/${MY_PLATFORM}/${MY_CONTAINER_NAME}/${MY_DOCKERFILE} \
+       ../../../../services/${MY_PLATFORM}/${MY_CONTAINER_NAME}
