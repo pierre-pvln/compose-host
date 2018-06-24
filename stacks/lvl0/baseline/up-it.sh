d96913a7ae04ac8b7bd3a1245768081b3a760d01
Pierre <pierre@pvln.nl>
2018-06-24 11:16:37 +0200

    update

--- stacks/lvl0/baseline/up-it.sh
+++ stacks/lvl0/baseline/up-it.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MY_DEFAULT_DATA_OVERRIDE="YES"
+MY_DEFAULT_DATA_OVERRIDE="NO"
 
 # Get relevant settings
 #
@@ -7,12 +7,18 @@ source ./settings.sh
 
 if [ -f ./default_data.sh ]
 then
-   if [ ${MY_DEFAULT_DATA_OVERRIDE}=="YES" ]
+   if [ ${MY_DEFAULT_DATA_OVERRIDE} == "YES" ]
    then
       # Add default data
       #
       source ./default_data.sh
+   else
+      echo "No default data override"
    fi
+
+else
+   echo "./default_data.sh not present"
+
 fi
 
 source ../../../d-scr/d-run.sh
