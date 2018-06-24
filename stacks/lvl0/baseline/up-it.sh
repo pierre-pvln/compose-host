198431ff3df2aaf2347634e6c3828822b2eecfc2
Pierre <pierre@pvln.nl>
2018-06-24 11:02:06 +0200

    updated

--- stacks/lvl0/baseline/up-it.sh
+++ stacks/lvl0/baseline/up-it.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+MY_DEFAULT_DATA_OVERRIDE="YES"
 
+# Get relevant settings
+#
 source ./settings.sh
+
+if [ -f ./default_data.sh ]
+then
+   if [ ${MY_DEFAULT_DATA_OVERRIDE}=="YES" ]
+   then
+      # Add default data
+      #
+      source ./default_data.sh
+   fi
+fi
 
 source ../../../d-scr/d-run.sh
