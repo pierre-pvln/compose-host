89180896172b742b1e4ee96f2181ab805d4bd4e8
Pierre Veelen <pierre@pvln.nl>
2018-06-25 12:22:38 +0200

    Update up-it.sh

--- stacks/lvl0/baseline/up-it.sh
+++ stacks/lvl0/baseline/up-it.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-MY_DEFAULT_DATA_OVERRIDE="NO"
+MY_DEFAULT_DATA_OVERRIDE="YES"
 
 # Get relevant settings
 #
 source ./settings.sh
 
+# Add/override data
+#
 if [ -f ./default_data.sh ]
 then
    if [ ${MY_DEFAULT_DATA_OVERRIDE} == "YES" ]
@@ -15,10 +17,31 @@ then
    else
       echo "No default data override"
    fi
-
 else
    echo "./default_data.sh not present"
-
 fi
 
-source ../../../d-scr/d-run.sh
+echo ===========
+echo Run
+echo
+echo ===========
+# inspiration: https://stackoverflow.com/questions/38576337/execute-bash-command-if-docker-container-does-not-exist
+#
+if [ ! "$(docker ps -q -f name=${MY_CONTAINER_NAME})" ]
+then
+    echo Container ${MY_CONTAINER_NAME} found
+    if [ "$(docker ps -aq -f status=exited -f status=created -f name=${MY_CONTAINER_NAME})" ]
+	then
+        echo Container status exited or created; so we have to remove it first
+        # cleanup container first
+        docker rm ${MY_CONTAINER_NAME}
+    fi
+
+    echo Running: docker run -it --name ${MY_CONTAINER_NAME} ${MY_BUILDER}/${MY_PLATFORM}-${MY_LEVEL}-${MY_CONTAINER_NAME}:${MY_VERSION}
+    echo
+    docker run -it \
+       --name ${MY_CONTAINER_NAME} \
+       ${MY_BUILDER}/${MY_PLATFORM}-${MY_LEVEL}-${MY_CONTAINER_NAME}:${MY_VERSION}
+else
+    echo Container ${MY_CONTAINER_NAME} not found
+fi
