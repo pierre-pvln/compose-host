cd7e4b21cb2ef592a4f7ba2dd442b55087380572
Pierre Veelen <pierre@pvln.nl>
2018-06-25 11:28:50 +0200

    Update default_data.sh

--- stacks/lvl1/php5/default_data.sh
+++ stacks/lvl1/php5/default_data.sh
@@ -2,6 +2,5 @@
 
 # copy default data to web data volume
 #
+echo "Copying default data to container volume"
 sudo cp --recursive ../../../../services/"${MY_PLATFORM}"/"${MY_CONTAINER_NAME}"/files/default/* "${MY_WEB_DATA_VOLUME}"
-
-#echo "running default_data.sh"
