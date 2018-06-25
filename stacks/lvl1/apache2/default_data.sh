1230e5a2f26011d849ac012c60e8c4b151ade99a
Pierre <pierre@pvln.nl>
2018-06-25 19:21:53 +0200

    update

--- stacks/lvl1/apache2/default_data.sh
+++ stacks/lvl1/apache2/default_data.sh
@@ -3,4 +3,4 @@
 # copy default data to web data volume
 #
 echo "Copying default data to container volume"
-cp --recursive ../../../../services/"${MY_PLATFORM}"/"${MY_CONTAINER_NAME}"/files/default/* "${MY_WEB_DATA_VOLUME}"
+sudo cp --recursive ../../../../services/"${MY_PLATFORM}"/"${MY_CONTAINER_NAME}"/files/default/* "${MY_WEB_DATA_VOLUME}"
