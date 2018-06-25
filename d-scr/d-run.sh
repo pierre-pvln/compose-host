04411d71eaf448d796921b86f21ad1dbade1b314
Pierre Veelen <pierre@pvln.nl>
2018-06-25 12:25:56 +0200

    Update d-run.sh

--- d-scr/d-run.sh
+++ d-scr/d-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo == NOT USED ANY MORE! ==
 echo
-echo lvl0 projects have their own run script.
-echo lvl1 and up use scripts in dc-scr folder.
+echo lvl0 projects have their own docker run script.
+echo lvl1 and up use docker-compose scripts in dc-scr folder.
