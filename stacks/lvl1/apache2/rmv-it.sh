093292bcdbccde192f7259bf86b784f10feef419
Pierre <pierre@pvln.nl>
2018-07-08 22:34:45 +0200

    update

--- stacks/lvl1/apache2/rmv-it.sh
+++ stacks/lvl1/apache2/rmv-it.sh
@@ -2,4 +2,50 @@
 
 source ./settings.sh
 
-source ../../../dc-scr/dc-rmv.sh
+echo
+echo "List all the container id's for: "${MY_PROJECT_NAME}
+echo "================================ "
+#echo
+#docker ps -a --filter=name=${MY_PROJECT_NAME}*
+MY_CONTAINERLIST=$(docker ps -a -q --filter=name=${MY_PROJECT_NAME}*)
+
+if [ ! -z ${MY_CONTAINERLIST} ]
+then
+   echo
+   echo "Stop the following containers:   "${MY_CONTAINERLIST}
+   echo "================================ "
+   #docker container ls -a --filter=id=${MY_CONTAINERLIST}
+   docker stop ${MY_CONTAINERLIST}
+
+   echo
+   echo "Remove the following containers: "${MY_CONTAINERLIST}
+   echo "================================ "
+   docker rm ${MY_CONTAINERLIST}
+
+   # to remove the container and the volumes in 1 run use
+   # docker rm --volumes ${MY_CONTAINERLIST}
+else
+   echo
+   echo "Containers already stopped and removed"
+   echo "================================ "
+fi
+
+
+echo
+echo "List all the docker volumes for: "${MY_PROJECT_NAME}
+echo "================================ "
+#docker volume ls -q --filter=name=${MY_PROJECT_NAME}*
+MY_VOLUMELIST=$(docker volume ls -q --filter=name=${MY_PROJECT_NAME}*)
+
+if [ ! -z ${MY_VOLUMELIST} ]
+then
+   echo
+   echo "And remove them"
+   echo "================================ "
+   docker volume rm ${MY_VOLUMELIST}
+else
+   echo
+   echo "Volumes already removed"
+   echo "================================ "
+fi
+
